5e72d7d2ac049dfed41614535577307dc504d0aa
Khelil Sator <khelil.sator.git@gmail.com>
2018-02-13 09:52:13 +0100

    Create configure_netbox.py

---
+++ configure_netbox.py
@@ -0,0 +1,65 @@
+###################################################
+# This script takes the variables defined in the file variables.yml and make rest calls to Netbox to configure it.
+###################################################
+###################################################
+# usage: python configure_netbox.py
+###################################################
+###################################################
+# This block indicates the various imports
+###################################################
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+from pprint import pprint
+import yaml
+import time
+
+
+##################################################
+# This block defines the functions we will use
+###################################################
+def import_variables_from_file():
+ my_variables_file=open('variables.yml', 'r')
+ my_variables_in_string=my_variables_file.read()
+ # print my_variables_in_string
+ my_variables_in_yaml=yaml.load(my_variables_in_string)
+ # print my_variables_in_yaml
+ # print my_variables_in_yaml['ip']
+ my_variables_file.close()
+ return my_variables_in_yaml
+
+
+def create_device-roles():
+ url=url_base + 'api/dcim/device-roles/'
+ for item in my_variables_in_yaml['device-roles']:
+     payload={
+         "name": item,
+         "slug": item,
+         "color": "2196f3"
+     }
+     rest_call = requests.post(url, headers=headers, data=json.dumps(payload))
+     #pprint (rest_call.json())
+     if rest_call.status_code == 201:
+         print my_variables_in_yaml['device-roles'][item] + ' device-roles created
+     else:
+         print 'failed to create device-roles ' + my_variables_in_yaml['device-roles'][item]
+     
+
+######################################################
+# this block is the Netbox configuration using REST calls
+######################################################
+my_variables_in_yaml=import_variables_from_file()
+
+url_base = 'http://' + my_variables_in_yaml['ip']
+
+token = my_variables_in_yaml['token']
+
+headers={
+    'Authorization': 'Token ' + token,
+    'Content-Type': 'application/json', 
+    'Accept': 'application/json'
+}
+
+create_device-roles()
+
+
