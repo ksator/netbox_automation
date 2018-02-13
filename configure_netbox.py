f0a3593269a7231ad957ebaf1b90f24e24f52f4d
ksator <khelil.sator.git@gmail.com>
2018-02-13 14:56:46 +0100

    m

--- configure_netbox.py
+++ configure_netbox.py
@@ -44,6 +44,57 @@ def create_device_roles():
      else:
          print 'failed to create device-roles ' + item
 
+def get_device_role_id(role):
+ url=url_base + 'api/dcim/device-roles/?name=' + role
+ rest_call = requests.get(url, headers=headers)
+ #pprint (rest_call.json())
+ #if rest_call.status_code != 200:
+ #    print 'failed to get the id of the role ' + role
+ role_id = rest_call.json()['results'][0]['id']
+ print role_id
+ return role_id
+
+def create_tenants():
+ url=url_base + 'api/tenancy/tenants/'
+ for item in my_variables_in_yaml['tenants']:
+     payload={
+         "name": item,
+         "slug": item
+     }
+     rest_call = requests.post(url, headers=headers, data=json.dumps(payload))
+     #pprint (rest_call.json())
+     if rest_call.status_code == 201:
+         print item + ' tenant created'
+     else:
+         print 'failed to create tenant ' + item
+
+def get_tenant_id(tenant):
+ url=url_base + 'api/tenancy/tenants/?name=' + tenant
+ rest_call = requests.get(url, headers=headers)
+ #pprint (rest_call.json())
+ #if rest_call.status_code != 200:
+ #    print 'failed to get the id of the tenant ' + tenant
+ tenant_id = rest_call.json()['results'][0]['id']
+ print tenant_id
+ return tenant_id
+
+def create_sites():
+ url=url_base + 'api/dcim/sites/'
+ tenant_id=get_tenant_id(my_variables_in_yaml['tenants'][0])
+ for item in my_variables_in_yaml['sites']:
+     payload={
+         "name": item,
+         "slug": item,
+         "tenant": tenant_id
+     }
+     rest_call = requests.post(url, headers=headers, data=json.dumps(payload))
+     #pprint (rest_call.json())
+     if rest_call.status_code == 201:
+         print item + ' site created'
+     else:
+         print 'failed to create site ' + item
+
+
 
 ######################################################
 # this block is the Netbox configuration using REST calls
@@ -62,4 +113,11 @@ headers={
 
 create_device_roles()
 
+get_device_role_id('spine_switch')
+
+create_tenants()
+
+get_tenant_id('evpn-vxlan-demo')
+
+create_sites()
 
